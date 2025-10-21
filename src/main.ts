78ad9e22c32e665f0a67cc0b3b7a3da4335521fb
Pedro Mattedi <mattedi.dev@gmail.com>
2025-10-20 21:31:09 -0300

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: ['http://localhost:5173', 'https://seufront.com'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  // });
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://seufront.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
